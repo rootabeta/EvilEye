bab5ef9911cb5402d32c30e6150eb7351df9f82a
rootabeta <rootabeta@protonmail.com>
2024-06-02 00:55:02 -0400

    Initial files

---
+++ EvilEye/EvilEye/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilEye
+{
+    internal class Program
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsUserAnAdmin();
+
+        static void Main(string[] args)
+        {
+            if (IsUserAnAdmin()) {
+                Console.WriteLine("Running in admin mode");
+            } else
+            {
+                Console.WriteLine("Running in user mode");
+            }
+        }
+    }
+}
